34ab393d9dccf4de7c4d241551f40f8c77e7cc18
Rebecca Stambler <rstambler@golang.org>
2020-05-11 17:12:20 -0400

    use hardcoded versions for tests
    Change-Id: If8deee45c1856a3ae93197074e44f312dd24ceff

--- test/gopls/update.test.ts
+++ test/gopls/update.test.ts
@@ -12,15 +12,12 @@ import { getTool, Tool } from '../../src/goTools';
 
 suite('gopls update tests', () => {
 	test('prompt for update', () => {
-		const latestRelease = semver.coerce('v0.4.0');
-		const latestReleaseTimestamp = moment('2020-04-08', 'YYYY-MM-DD');
-		const latestPrerelease = semver.coerce('v0.4.0-pre1');
-		const latestPrereleaseTimestamp = moment('2020-05-07', 'YYYY-MM-DD');
+		const tool = getTool('gopls');
 		const testCases: [string, boolean, semver.SemVer][] = [
-			['v0.3.1', false, latestRelease], // outdated tagged version
-			['v0.3.1', true, latestPrerelease], // outdated tagged version, accept pre-release
+			['v0.3.1', false, tool.latestVersion], // outdated tagged version
+			['v0.3.1', true, tool.latestPrereleaseVersion], // outdated tagged version, accept pre-release
 			['v0.4.0', false, null], // up-to-date tagged version
-			['v0.4.0', true, latestPrerelease], // up-to-date tagged version, accept pre-release
+			['v0.4.0', true, tool.latestPrereleaseVersion], // up-to-date tagged version, accept pre-release
 			['(devel)', false, null], // developer version
 			['(devel)', true, null], // developer version, accept pre-release
 			['nosuchversion', false, null], // nonsense version
@@ -34,11 +31,11 @@ suite('gopls update tests', () => {
 			],
 			[
 				'v0.3.1-pre1 h1:pBnJjmdcHy5AiRJleOWaakxFHykf8uXzSZKQMd0EA0Q=',
-				false, latestRelease // outdated pre-release version, don't accept pre-release
+				false, tool.latestVersion // outdated pre-release version, don't accept pre-release
 			],
 			[
 				'v0.3.1-pre1 h1:pBnJjmdcHy5AiRJleOWaakxFHykf8uXzSZKQMd0EA0Q=',
-				true, latestPrerelease // outdated pre-release version, accept pre-release
+				true, tool.latestPrereleaseVersion // outdated pre-release version, accept pre-release
 			],
 			[
 				'v0.0.0-20200509030707-2212a7e161a5 h1:0gSpZ0Z2URJoo3oilGRq9ViMLDTlmNSDCyeZNHHrvd4=',
@@ -46,15 +43,15 @@ suite('gopls update tests', () => {
 			],
 			[
 				'v0.0.0-20200501030707-2212a7e161a5 h1:0gSpZ0Z2URJoo3oilGRq9ViMLDTlmNSDCyeZNHHrvd4=',
-				true, latestPrerelease, // recent pseudoversion (before pre-release)
+				true, tool.latestPrereleaseVersion, // recent pseudoversion (before pre-release)
 			],
 			[
 				'v0.0.0-20200309030707-2212a7e161a5 h1:0gSpZ0Z2URJoo3oilGRq9ViMLDTlmNSDCyeZNHHrvd4=',
-				false, latestRelease, // old pseudoversion
+				false, tool.latestVersion, // old pseudoversion
 			],
 			[
 				'v0.0.0-20200309030707-2212a7e161a5 h1:0gSpZ0Z2URJoo3oilGRq9ViMLDTlmNSDCyeZNHHrvd4=',
-				true, latestPrerelease, // old pseudoversion
+				true, tool.latestPrereleaseVersion, // old pseudoversion
 			],
 		];
 		testCases.map(async ([usersVersion, acceptPrerelease, want], i) => {
@@ -63,19 +60,18 @@ suite('gopls update tests', () => {
 			});
 			sinon.replace(lsp, 'getLatestGoplsVersion', async () => {
 				if (acceptPrerelease) {
-					return latestPrerelease;
+					return tool.latestPrereleaseVersion;
 				}
-				return latestRelease;
+				return tool.latestVersion;
 			});
 			sinon.replace(lsp, 'getTimestampForVersion', async (_: Tool, version: semver.SemVer) => {
-				if (version === latestRelease) {
-					return latestReleaseTimestamp;
+				if (version === tool.latestVersion) {
+					return tool.latestVersionTimestamp;
 				}
-				if (version === latestPrerelease) {
-					return latestPrereleaseTimestamp;
+				if (version === tool.latestPrereleaseVersion) {
+					return tool.latestPrereleaseVersionTimestamp;
 				}
 			});
-			const tool = getTool('gopls');
 			const got = await lsp.shouldUpdateLanguageServer(tool, 'bad/path/to/gopls', true);
 			assert.equal(got, want);
 			sinon.restore();
