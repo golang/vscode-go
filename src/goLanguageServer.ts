2bde89c5bf6cf30b37a4db094a3e84299d00efe6
Rebecca Stambler <rstambler@golang.org>
2020-05-12 02:00:54 +0000

    test/gopls: add tests for the gopls update logic
    This has been untested until now, so add some basic tests that confirm we handle most cases. Additional tests are probably needed for the helper functions.
    
    Change-Id: Iee879bc2c7d7d7ff1f44230c80b85ac3dffdf28c
    
    Change-Id: Iee879bc2c7d7d7ff1f44230c80b85ac3dffdf28c
    GitHub-Last-Rev: 09a323f87c863579b9d451817613ce618af69d87
    GitHub-Pull-Request: golang/vscode-go#29
    Reviewed-on: https://go-review.googlesource.com/c/vscode-go/+/233158
    Reviewed-by: Hyang-Ah Hana Kim <hyangah@gmail.com>

--- src/goLanguageServer.ts
+++ src/goLanguageServer.ts
@@ -403,7 +403,7 @@ function allFoldersHaveSameGopath(): boolean {
 }
 
 const acceptGoplsPrerelease = true;  // For nightly, we accept the prerelease version.
-async function shouldUpdateLanguageServer(
+export async function shouldUpdateLanguageServer(
 	tool: Tool,
 	languageServerToolPath: string,
 	makeProxyCall: boolean
@@ -414,7 +414,7 @@ async function shouldUpdateLanguageServer(
 	}
 
 	// First, run the "gopls version" command and parse its results.
-	const usersVersion = await goplsVersion(languageServerToolPath);
+	const usersVersion = await getLocalGoplsVersion(languageServerToolPath);
 
 	// We might have a developer version. Don't make the user update.
 	if (usersVersion === '(devel)') {
@@ -422,7 +422,7 @@ async function shouldUpdateLanguageServer(
 	}
 
 	// Get the latest gopls version. If it is for nightly, using the prereleased version is ok.
-	let latestVersion = makeProxyCall ? await latestGopls(tool) : tool.latestVersion;
+	let latestVersion = makeProxyCall ? await getLatestGoplsVersion(tool) : tool.latestVersion;
 
 	// If we failed to get the gopls version, pick the one we know to be latest at the time of this extension's last update
 	if (!latestVersion) {
@@ -438,10 +438,10 @@ async function shouldUpdateLanguageServer(
 
 	// The user may have downloaded golang.org/x/tools/gopls@master,
 	// which means that they have a pseudoversion.
-	const usersTime = parsePseudoversionTimestamp(usersVersion);
+	const usersTime = parseTimestampFromPseudoversion(usersVersion);
 	// If the user has a pseudoversion, get the timestamp for the latest gopls version and compare.
 	if (usersTime) {
-		let latestTime = makeProxyCall ? await goplsVersionTimestamp(tool, latestVersion) : tool.latestVersionTimestamp;
+		let latestTime = makeProxyCall ? await getTimestampForVersion(tool, latestVersion) : tool.latestVersionTimestamp;
 		if (!latestTime) {
 			latestTime = tool.latestVersionTimestamp;
 		}
@@ -453,13 +453,13 @@ async function shouldUpdateLanguageServer(
 	return semver.lt(usersVersion, latestVersion) ? latestVersion : null;
 }
 
-// Copied from src/cmd/go/internal/modfetch.
+// Copied from src/cmd/go/internal/modfetch.go.
 const pseudoVersionRE = /^v[0-9]+\.(0\.0-|\d+\.\d+-([^+]*\.)?0\.)\d{14}-[A-Za-z0-9]+(\+incompatible)?$/;
 
-// parsePseudoVersion reports whether v is a pseudo-version.
-// The timestamp is the center component, and it has the format "YYYYMMDDHHmmss".
-
-function parsePseudoversionTimestamp(version: string): moment.Moment {
+// parseTimestampFromPseudoversion returns the timestamp for the given
+// pseudoversion. The timestamp is the center component, and it has the
+// format "YYYYMMDDHHmmss".
+function parseTimestampFromPseudoversion(version: string): moment.Moment {
 	const split = version.split('-');
 	if (split.length < 2) {
 		return null;
@@ -495,7 +495,7 @@ function parsePseudoversionTimestamp(version: string): moment.Moment {
 	return moment.utc(timestamp, 'YYYYMMDDHHmmss');
 }
 
-async function goplsVersionTimestamp(tool: Tool, version: semver.SemVer): Promise<moment.Moment> {
+export async function getTimestampForVersion(tool: Tool, version: semver.SemVer): Promise<moment.Moment> {
 	const data = await goProxyRequest(tool, `v${version.format()}.info`);
 	if (!data) {
 		return null;
@@ -504,7 +504,7 @@ async function goplsVersionTimestamp(tool: Tool, version: semver.SemVer): Promis
 	return time;
 }
 
-async function latestGopls(tool: Tool): Promise<semver.SemVer> {
+export async function getLatestGoplsVersion(tool: Tool): Promise<semver.SemVer> {
 	// If the user has a version of gopls that we understand,
 	// ask the proxy for the latest version, and if the user's version is older,
 	// prompt them to update.
@@ -535,7 +535,10 @@ async function latestGopls(tool: Tool): Promise<semver.SemVer> {
 	return versions.find((version) => !version.prerelease || !version.prerelease.length);
 }
 
-async function goplsVersion(goplsPath: string): Promise<string> {
+// getLocalGoplsVersion returns the version of gopls that is currently
+// installed on the user's machine. This is determined by running the
+// `gopls version` command.
+export async function getLocalGoplsVersion(goplsPath: string): Promise<string> {
 	const env = getToolsEnvVars();
 	const execFile = util.promisify(cp.execFile);
 	let output: any;
@@ -592,9 +595,15 @@ async function goplsVersion(goplsPath: string): Promise<string> {
 }
 
 async function goProxyRequest(tool: Tool, endpoint: string): Promise<any> {
-	const proxies = goProxy();
+	// Get the user's value of GOPROXY.
+	// If it is not set, we cannot make the request.
+	const output: string = process.env['GOPROXY'];
+	if (!output || !output.trim()) {
+		return null;
+	}
 	// Try each URL set in the user's GOPROXY environment variable.
 	// If none is set, don't make the request.
+	const proxies = output.trim().split(',|');
 	for (const proxy of proxies) {
 		if (proxy === 'direct') {
 			continue;
@@ -611,13 +620,4 @@ async function goProxyRequest(tool: Tool, endpoint: string): Promise<any> {
 		return data;
 	}
 	return null;
-}
-
-function goProxy(): string[] {
-	const output: string = process.env['GOPROXY'];
-	if (!output || !output.trim()) {
-		return [];
-	}
-	const split = output.trim().split(',');
-	return split;
 }
---
+++ test/gopls/update.test.ts
@@ -0,0 +1,94 @@
+/*---------------------------------------------------------
+ * Copyright (C) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------*/
+
+import * as assert from 'assert';
+import moment = require('moment');
+import semver = require('semver');
+import sinon = require('sinon');
+import lsp = require('../../src/goLanguageServer');
+import { getTool, Tool } from '../../src/goTools';
+
+suite('gopls update tests', () => {
+	test('prompt for update', () => {
+		const tool = getTool('gopls');
+		const testCases: [string, string, boolean, semver.SemVer][] = [
+			['outdated, tagged', 'v0.3.1', false, tool.latestVersion],
+			['outdated, tagged (pre-release)', '0.3.1', true, tool.latestPrereleaseVersion],
+			['up-to-date, tagged', 'v0.4.0', false, null],
+			['up-to-date tagged (pre-release)', 'v0.4.0', true, tool.latestPrereleaseVersion],
+			['developer version', '(devel)', false, null],
+			['developer version (pre-release)', '(devel)', true, null],
+			['nonsense version', 'nosuchversion', false, null],
+			['nonsense version (pre-release)', 'nosuchversion', true, null],
+			[
+				'latest pre-release',
+				'v0.4.1-pre1 h1:w6e4AmFe6sDSVrgaRkf4WqLyVAlByUrr9QM5xH7z1e4=',
+				false, null,
+			],
+			[
+				'latest pre-release (pre-release)',
+				'v0.4.1-pre1 h1:w6e4AmFe6sDSVrgaRkf4WqLyVAlByUrr9QM5xH7z1e4=',
+				true, null,
+			],
+			[
+				'outdated pre-release version',
+				'v0.3.1-pre1 h1:pBnJjmdcHy5AiRJleOWaakxFHykf8uXzSZKQMd0EA0Q=',
+				false, tool.latestVersion,
+			],
+			[
+				'outdated pre-release version (pre-release)',
+				'v0.3.1-pre1 h1:pBnJjmdcHy5AiRJleOWaakxFHykf8uXzSZKQMd0EA0Q=',
+				true, tool.latestPrereleaseVersion,
+			],
+			[
+				'recent pseudoversion after pre-release',
+				'v0.0.0-20200509030707-2212a7e161a5 h1:0gSpZ0Z2URJoo3oilGRq9ViMLDTlmNSDCyeZNHHrvd4=',
+				false, null,
+			],
+			[
+				'recent pseudoversion before pre-release',
+				'v0.0.0-20200501030707-2212a7e161a5 h1:0gSpZ0Z2URJoo3oilGRq9ViMLDTlmNSDCyeZNHHrvd4=',
+				false, null,
+			],
+			[
+				'recent pseudoversion before pre-release (pre-release)',
+				'v0.0.0-20200501030707-2212a7e161a5 h1:0gSpZ0Z2URJoo3oilGRq9ViMLDTlmNSDCyeZNHHrvd4=',
+				true, tool.latestPrereleaseVersion,
+			],
+			[
+				'outdated pseudoversion',
+				'v0.0.0-20200309030707-2212a7e161a5 h1:0gSpZ0Z2URJoo3oilGRq9ViMLDTlmNSDCyeZNHHrvd4=',
+				false, tool.latestVersion,
+			],
+			[
+				'outdated pseudoversion (pre-release)',
+				'v0.0.0-20200309030707-2212a7e161a5 h1:0gSpZ0Z2URJoo3oilGRq9ViMLDTlmNSDCyeZNHHrvd4=',
+				true, tool.latestPrereleaseVersion,
+			],
+		];
+		testCases.map(async ([name, usersVersion, acceptPrerelease, want], i) => {
+			sinon.replace(lsp, 'getLocalGoplsVersion', async () => {
+				return usersVersion;
+			});
+			sinon.replace(lsp, 'getLatestGoplsVersion', async () => {
+				if (acceptPrerelease) {
+					return tool.latestPrereleaseVersion;
+				}
+				return tool.latestVersion;
+			});
+			sinon.replace(lsp, 'getTimestampForVersion', async (_: Tool, version: semver.SemVer) => {
+				if (version === tool.latestVersion) {
+					return tool.latestVersionTimestamp;
+				}
+				if (version === tool.latestPrereleaseVersion) {
+					return tool.latestPrereleaseVersionTimestamp;
+				}
+			});
+			const got = await lsp.shouldUpdateLanguageServer(tool, 'bad/path/to/gopls', true);
+			assert.equal(got, want, `${name}@${i} failed`);
+			sinon.restore();
+		});
+	});
+});
