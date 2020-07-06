0efae5711f797147d7f561c513c19adf8600ffe2
OneOfOne <oneofone@gmail.com>
2020-07-06 18:41:17 -0500

    fix the debugger and mocha warnings

--- webpack.config.js
+++ webpack.config.js
@@ -7,14 +7,18 @@ const path = require('path');
 /**@type {import('webpack').WebpackOptions}*/
 const config = {
 	target: 'node',
-	entry: './src/goMain.ts',
+	entry: {
+		goMain: './src/goMain.ts',
+		debugAdapter: './src/debugAdapter/goDebug.ts',
+		debugAdapter2: './src/debugAdapter2/goDlvDebugMain.ts',
+	},
 	output: {
 		path: path.resolve(__dirname, 'dist'),
-		filename: 'extension.js',
+		filename: '[name].js',
 		libraryTarget: 'commonjs2',
 		devtoolModuleFilenameTemplate: '../[resource-path]'
 	},
-	devtool: 'source-map',
+	devtool: 'none',
 	externals: {
 		// the vscode-module is created on-the-fly and must be excluded.
 		vscode: 'commonjs vscode'
@@ -34,6 +38,10 @@ const config = {
 				]
 			}
 		]
-	}
+	},
+	optimization: {
+		// when this is true, the debugger breaks...
+		minimize: false
+	},
 };
 module.exports = config;
