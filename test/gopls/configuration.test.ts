f38e34851acc2d6e4f7fce1ed34670941330e57d
Hana <hyangah@gmail.com>
2020-12-16 14:42:07 -0500

    src/goLanguageServer: propagate go.buildFlags,buildTags to gopls
    They are passed as gopls.buildFlags only if gopls.buildFlags isn't
    already explicitly set - users have to use JSON-based UI and add
    the fields (including []).
    
    If go.buildFlags has -tags, go.buildTags is ignored, which is
    consistent with the extension's build behavior coded in goBuild
    (goBuild.ts).
    
    For golang/vscode-go#155
    
    Change-Id: I03d5f5eb58d049454fb4a66d902a7daea4a1269f
    Reviewed-on: https://go-review.googlesource.com/c/vscode-go/+/273146
    Trust: Hyang-Ah Hana Kim <hyangah@gmail.com>
    Trust: Suzy Mueller <suzmue@golang.org>
    Run-TryBot: Hyang-Ah Hana Kim <hyangah@gmail.com>
    TryBot-Result: kokoro <noreply+kokoro@google.com>
    Reviewed-by: Suzy Mueller <suzmue@golang.org>

--- test/gopls/configuration.test.ts
+++ test/gopls/configuration.test.ts
@@ -12,27 +12,33 @@ import * as lsp from '../../src/goLanguageServer';
 import { getTool, Tool } from '../../src/goTools';
 
 suite('gopls configuration tests', () => {
-	test('configuration', async () => {
+	test('filterGoplsDefaultConfigValues', async () => {
 		const defaultGoplsConfig = vscode.workspace.getConfiguration('gopls');
 		const defaultGoplsAnalysesConfig = vscode.workspace.getConfiguration('gopls.analyses');
 
 		interface TestCase {
 			name: string;
 			section: string;
-			base: any;
+			input: any;
 			want: any;
 		}
 		const testCases: TestCase[] = [
 			{
 				name: 'user set no gopls settings',
 				section: 'gopls',
-				base: defaultGoplsConfig,
+				input: defaultGoplsConfig,
 				want: {}
 			},
 			{
 				name: 'user set some gopls settings',
 				section: 'gopls',
-				base: defaultGoplsConfig,
+				input: Object.assign({}, defaultGoplsConfig, {
+					buildFlags: ['-something'],
+					env: { foo: 'bar' },
+					hoverKind: 'NoDocumentation',
+					usePlaceholders: true,
+					linkTarget: 'godoc.org'
+				}),
 				want: {
 					buildFlags: ['-something'],
 					env: { foo: 'bar' },
@@ -42,37 +48,119 @@ suite('gopls configuration tests', () => {
 				},
 			},
 			{
-				name: 'gopls asks analyses section, user set no analysis',
-				section: 'gopls.analyses',
-				base: defaultGoplsAnalysesConfig,
-				want: {},
-			},
-			{
-				name: 'gopls asks analyses section, user set some',
-				section: 'gopls.analyses',
-				base: defaultGoplsAnalysesConfig,
-				want: {
-					coolAnalysis: true,
-				},
-			},
-			{
 				name: 'user set extra gopls settings',
 				section: 'gopls',
-				base: defaultGoplsConfig,
+				input: Object.assign({}, defaultGoplsConfig, {
+					undefinedGoplsSetting: true
+				}),
 				want: {
 					undefinedGoplsSetting: true,
 				},
 			},
 			{
-				name: 'gopls asks undefined config section',
+				name: 'never returns undefined',
 				section: 'undefined.section',
-				base: undefined,
+				input: undefined,
 				want: {},
-			}
+			},
 		];
 		testCases.map((tc: TestCase) => {
-			const input = Object.assign({}, tc.base, tc.want);
-			const actual = lsp.filterDefaultConfigValues(input, tc.section, undefined);
+			const actual = lsp.filterGoplsDefaultConfigValues(tc.input, undefined);
+			assert.deepStrictEqual(actual, tc.want, `Failed: ${tc.name}`);
+		});
+	});
+
+	test('passGoConfigToGoplsConfigValues', async () => {
+		interface TestCase {
+			name: string;
+			goplsConfig: any;
+			goConfig: any;
+			want: any;
+		}
+		const testCases: TestCase[] = [
+			{
+				name: 'undefined gopls, go configs result in an empty config',
+				goplsConfig: undefined,
+				goConfig: undefined,
+				want: {}
+			},
+			{
+				name: 'empty gopls, go configs result in an empty config',
+				goplsConfig: {},
+				goConfig: {},
+				want: {}
+			},
+			{
+				name: 'empty gopls, default go configs result in an empty config',
+				goplsConfig: {},
+				goConfig: {
+					buildFlags: [],
+					buildTags: '',
+				},
+				want: {}
+			},
+			{
+				name: 'pass go config buildFlags to gopls config',
+				goplsConfig: {},
+				goConfig: { buildFlags: ['-modfile', 'gopls.mod', '-tags', 'tag1,tag2', '-modcacherw'] },
+				want: { buildFlags: ['-modfile', 'gopls.mod', '-tags', 'tag1,tag2', '-modcacherw']}
+			},
+			{
+				name: 'pass go config buildTags to gopls config',
+				goplsConfig: {},
+				goConfig: { buildTags: 'tag1,tag2' },
+				want: { buildFlags: ['-tags', 'tag1,tag2']}
+			},
+			{
+				name: 'do not pass go config buildTags if buildFlags already have tags',
+				goplsConfig: {},
+				goConfig: {
+					buildFlags: ['-tags', 'tag0'],
+					buildTags: 'tag1,tag2'
+				},
+				want: { buildFlags: ['-tags', 'tag0']}
+			},
+			{
+				name: 'do not mutate other gopls config but gopls.buildFlags',
+				goplsConfig: {
+					env: {GOPROXY: 'direct'}
+				},
+				goConfig: { buildFlags: ['-modfile', 'gopls.mod', '-tags', 'tag1,tag2', '-modcacherw'] },
+				want: {
+					env: { GOPROXY : 'direct' },
+					buildFlags: ['-modfile', 'gopls.mod', '-tags', 'tag1,tag2', '-modcacherw']}
+			},
+
+			{
+				name: 'do not mutate misconfigured gopls.buildFlags',
+				goplsConfig: {
+					buildFlags: '-modfile gopls.mod',  // misconfiguration
+				},
+				goConfig: {
+					buildFlags: '-modfile go.mod -tags tag1 -modcacherw',
+				},
+				want: { buildFlags: '-modfile gopls.mod' }
+			},
+			{
+				name: 'do not overwrite gopls config if it is explicitly set',
+				goplsConfig: {
+					env: {GOPROXY: 'direct'},
+					buildFlags: [],  // empty
+				},
+				goConfig: {
+					// expect only non-conflicting flags (tags, modcacherw) passing.
+					buildFlags: ['-modfile go.mod -tags tag1 -modcacherw'],
+					buildTags: 'tag3',
+				},
+				want: {
+					env: {GOPROXY: 'direct'},
+					buildFlags: [],
+				}  // gopls.buildFlags untouched.
+			},
+
+		];
+		testCases.map((tc: TestCase) => {
+			const actual = lsp.passGoConfigToGoplsConfigValues(tc.goplsConfig, tc.goConfig);
 			assert.deepStrictEqual(actual, tc.want, `Failed: ${tc.name}`);
 		});
 	});
