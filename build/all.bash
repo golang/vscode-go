a7d4839e0eb5225d39cb06642b1a4d9becfcb5fc
Hana <hyangah@gmail.com>
2021-01-28 16:41:59 -0500

    [release] build: run docker with SYS_PTRACE capability
    Debug tests need to launch dlv and that needs ptrace.
    SYS_PTRACE is not in the default capability list so, add it.
    https://docs.docker.com/engine/reference/run/
    
    Change-Id: I3c53e0dd956b731726c5bbf7853d3f30d258dd26
    Reviewed-on: https://go-review.googlesource.com/c/vscode-go/+/287712
    Trust: Hyang-Ah Hana Kim <hyangah@gmail.com>
    Run-TryBot: Hyang-Ah Hana Kim <hyangah@gmail.com>
    Reviewed-by: Robert Findley <rfindley@google.com>
    TryBot-Result: kokoro <noreply+kokoro@google.com>
    (cherry picked from commit ad3a7d2bf3c6815d77b3ebff1d349e8ca1efe3ee)
    Reviewed-on: https://go-review.googlesource.com/c/vscode-go/+/287798

--- build/all.bash
+++ build/all.bash
@@ -60,7 +60,8 @@ run_test() {
 run_test_in_docker() {
   echo "**** Building the docker image ***"
   docker build -t vscode-test-env -f ./build/Dockerfile .
-  docker run --workdir=/workspace -v "$(pwd):/workspace" vscode-test-env ci
+  # For debug tests, we need ptrace.
+  docker run --cap-add SYS_PTRACE --workdir=/workspace -v "$(pwd):/workspace" vscode-test-env ci
 }
 
 prepare_nightly() {
