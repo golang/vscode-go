1d549a24b0d1386ce644a53ee3b2c8e3b7c3b1aa
Hana <hyangah@gmail.com>
2021-04-08 12:06:47 -0400

    src/goInstallTools: linkify dlv-dap installation instruction
    Change-Id: I666ce50c8a9230be3e2fe447bd6e8db11328adea
    Reviewed-on: https://go-review.googlesource.com/c/vscode-go/+/308492
    Trust: Hyang-Ah Hana Kim <hyangah@gmail.com>
    Run-TryBot: Hyang-Ah Hana Kim <hyangah@gmail.com>
    TryBot-Result: kokoro <noreply+kokoro@google.com>
    Reviewed-by: Suzy Mueller <suzmue@golang.org>

--- src/goInstallTools.ts
+++ src/goInstallTools.ts
@@ -357,8 +357,8 @@ export async function promptForMissingTool(toolName: string) {
 	let msg = `The "${tool.name}" command is not available.
 Run "go get -v ${getImportPath(tool, goVersion)}" to install.`;
 	if (tool.name === 'dlv-dap') {
-		msg = `The "${tool.name}" command is not available.
-Installation instructions can be found at https://github.com/golang/vscode-go/blob/master/docs/dlv-dap.md#updating-dlv-dap.`;
+		msg = `The ["${tool.name}"](https://github.com/golang/vscode-go/blob/master/docs/dlv-dap.md) command is not available.
+Please select "Install", or follow the installation instructions [here](https://github.com/golang/vscode-go/blob/master/docs/dlv-dap.md#updating-dlv-dap).`;
 	}
 	const selected = await vscode.window.showErrorMessage(msg, ...installOptions);
 	switch (selected) {
