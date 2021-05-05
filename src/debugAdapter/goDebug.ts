e8dc6c4fa7e3f8cba2cf164c448d346c561a38a6
Hana <hyangah@gmail.com>
2021-05-04 21:56:08 -0400

    package.json: remove 'init' debug attribute
    We use dlv in headless mode and dlv headless server always ignores --init flag.
    This seemed to be added in early days of vscode-go to support all delve flags
    available back then, and it never worked.
    
    Change-Id: Id8134b6b7c0ad119fc97862754925bd8d3464b54
    Reviewed-on: https://go-review.googlesource.com/c/vscode-go/+/317150
    Trust: Hyang-Ah Hana Kim <hyangah@gmail.com>
    Run-TryBot: Hyang-Ah Hana Kim <hyangah@gmail.com>
    TryBot-Result: kokoro <noreply+kokoro@google.com>
    Reviewed-by: Polina Sokolova <polina@google.com>

--- src/debugAdapter/goDebug.ts
+++ src/debugAdapter/goDebug.ts
@@ -649,9 +649,6 @@ export class Delve {
 				if (launchArgs.buildFlags) {
 					dlvArgs.push('--build-flags=' + launchArgs.buildFlags);
 				}
-				if (launchArgs.init) {
-					dlvArgs.push('--init=' + launchArgs.init);
-				}
 				if (launchArgs.backend) {
 					dlvArgs.push('--backend=' + launchArgs.backend);
 				}
