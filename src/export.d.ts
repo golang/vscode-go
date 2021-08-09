0171541d958246fc59056e4eba75cd4aa940976f
Ethan Reesor <ethan.reesor@gmail.com>
2021-08-09 17:18:12 -0500

    src/extensionAPI: add copyright

--- src/export.d.ts
+++ src/export.d.ts
@@ -1,3 +1,8 @@
+/*---------------------------------------------------------
+ * Copyright 2021 The Go Authors. All rights reserved.
+ * Licensed under the MIT License. See LICENSE in the project root for license information.
+ *--------------------------------------------------------*/
+
 import { Uri } from 'vscode';
 export { ToolAtVersion } from './goTools';
 
--- src/extensionAPI.ts
+++ src/extensionAPI.ts
@@ -1,3 +1,8 @@
+/*---------------------------------------------------------
+ * Copyright 2021 The Go Authors. All rights reserved.
+ * Licensed under the MIT License. See LICENSE in the project root for license information.
+ *--------------------------------------------------------*/
+
 import { Uri } from 'vscode';
 import { CommandInvocation, ExtensionAPI } from './export';
 import { getBinPathWithExplanation } from './util';
