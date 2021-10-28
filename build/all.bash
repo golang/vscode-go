681be58c0357a89f6133e1e0b678b3d819ca648b
Hana <hyangah@gmail.com>
2021-10-28 08:49:18 -0400

    tools/installtools: add a helper to install Go tools for testing
    GitHub Action workflows had tool installation commands defined
    in .yml file while Kokoro CI used a bash script. This change
    made them to share the same logic.
    
    Instead of using the existing build/all.bash script, this CL
    reimplemented the installation logic in go
    
    - to have one single program for all platforms (linux, windows, mac)
    - to implement complex logic that switches between go get and
    go install depending on the go version (versioned binary installation
    using go install became available only after 1.16.
    
    Updates golang/vscode-go#1825
    
    Change-Id: I295856e7d2bdd2d1fc07efc1e794eccb42b55ad6
    Reviewed-on: https://go-review.googlesource.com/c/vscode-go/+/359397
    Trust: Hyang-Ah Hana Kim <hyangah@gmail.com>
    Run-TryBot: Hyang-Ah Hana Kim <hyangah@gmail.com>
    TryBot-Result: kokoro <noreply+kokoro@google.com>
    Reviewed-by: Suzy Mueller <suzmue@golang.org>

--- build/all.bash
+++ build/all.bash
@@ -92,36 +92,6 @@ prepare_nightly() {
   cp build/nightly/const.ts src/const.ts
 }
 
-# setup dependencies required for tests.
-install_dependencies() {
-	# TARGET is where `go get` will output the compiled binaries.
-	local GOPATHS=`go env GOPATH`
-	local TARGET="${GOBIN}"
-	if [[ -z "${GOBIN}" ]]; then TARGET="${GOPATHS%%:*}/bin" ; fi
-
-	GO111MODULE=on go install golang.org/x/tools/gopls@latest
-	GO111MODULE=on go install github.com/acroca/go-symbols@latest
-	GO111MODULE=on go install github.com/cweill/gotests/gotests@latest
-	GO111MODULE=on go install github.com/davidrjenni/reftools/cmd/fillstruct@latest
-	GO111MODULE=on go install github.com/haya14busa/goplay/cmd/goplay@latest
-
-	# We install two versions of gocode, one for module mode (gocode-gomod)
-	# and another for GOPATH mode (gocode).
-	GO111MODULE=on go install github.com/stamblerre/gocode@latest && mv "${TARGET}/gocode" "${TARGET}/gocode-gomod"
-	GO111MODULE=on go install github.com/mdempsky/gocode@latest
-
-	GO111MODULE=on go install github.com/ramya-rao-a/go-outline@latest
-	GO111MODULE=on go install github.com/rogpeppe/godef@latest
-	GO111MODULE=on go install github.com/sqs/goreturns@latest
-	GO111MODULE=on go install github.com/uudashr/gopkgs/v2/cmd/gopkgs@latest
-	GO111MODULE=on go install github.com/zmb3/gogetdoc@latest
-	GO111MODULE=on go install honnef.co/go/tools/cmd/staticcheck@latest
-	GO111MODULE=on go install golang.org/x/tools/cmd/gorename@latest
-
-	GO111MODULE=on go install github.com/go-delve/delve/cmd/dlv@master && cp "${TARGET}/dlv" "${TARGET}/dlv-dap"
-	GO111MODULE=on go install github.com/go-delve/delve/cmd/dlv@latest
-}
-
 main() {
   cd "$(root_dir)"  # always run from the script root.
   case "$1" in
@@ -143,9 +113,6 @@ main() {
       ;;
     "prepare_nightly")
       prepare_nightly
-      ;;
-    "setup_env")
-      install_dependencies
       ;;
     *)
       usage
