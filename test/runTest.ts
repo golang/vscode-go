3e640751f213229a760f2c2c96651e42208cb7c1
Hana <hyangah@gmail.com>
2022-01-10 13:44:26 -0500

    test: use @vscode/test-electron instead of vscode-test
    https://code.visualstudio.com/api/working-with-extensions/testing-extension
    
    @vscode/test-electron is the same as vscode-test
    (https://github.com/microsoft/vscode-test)
    but a new name (renamed in mid 2021)
    
    Change-Id: I2ef76aef28c758a1f1157c395f3ce5ecc63a3ef5
    Reviewed-on: https://go-review.googlesource.com/c/vscode-go/+/377336
    Trust: Hyang-Ah Hana Kim <hyangah@gmail.com>
    Run-TryBot: Hyang-Ah Hana Kim <hyangah@gmail.com>
    TryBot-Result: kokoro <noreply+kokoro@google.com>
    Reviewed-by: Suzy Mueller <suzmue@golang.org>

--- test/runTest.ts
+++ test/runTest.ts
@@ -1,7 +1,7 @@
 /* eslint-disable no-process-exit */
 /* eslint-disable node/no-unpublished-import */
 import * as path from 'path';
-import { runTests } from 'vscode-test';
+import { runTests } from '@vscode/test-electron';
 
 async function main() {
 	// We are in test mode.
