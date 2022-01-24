c845547072ecd493f436fa12b2334bca0bb0867d
Hana <hyangah@gmail.com>
2022-01-24 12:21:41 -0500

    [release] src/goTest: remove '#' in subtest name parsing cutset
    Subtests are detected dynamically - while running tests.
    GoTestRunner heuristically builds subtest trees by parsing
    the test names observed while running tests.
    
    Before this CL, it used both '/' and '#' as the separator for
    parsing.  For example, it placed a test item for Foo as a
    child of TestXxx when it observed TestXxx/Foo, and a test item
    01 as a child of TestXxx if it saw TestXxx#01.
    
    However, go test uses '#' to resolve conflicts in subtest names,
    so it's not right to assume TestXXX#01 is a subtest of TestXXX.
    
    Moreover, treating TestXXX/subtest#01 or TestXXX/subtest#02 as
    a subtest of TestXXX/subtest confuses the text UI API because
    `go test -json` will generate event sequence like
    
    TestXXX run
     TestXXX/subtest run
     TestXXX/subtest#01 run
     TestXXX/subtest#02 run
     TestXXX/subtest pass
     TestXXX/subtest#01 pass
     TestXXX/subtest#02 pass
    TestXXX pass
    
    That causes the test UI to show only the last item for TestXXX/subtest#02
    to appear in TestXXX/subtest's child.
    
    This bug also makes Fuzz test's crasher and seed case listing confusing.
    
    This CL removes '#' from the cutset.
    
    Fixes golang/vscode-go#2023
    For golang/vscode-go#1922
    
    Change-Id: I33ba5c17e9095686a87c719d44fe7330269d9cc3
    Reviewed-on: https://go-review.googlesource.com/c/vscode-go/+/380501
    Trust: Hyang-Ah Hana Kim <hyangah@gmail.com>
    Run-TryBot: Hyang-Ah Hana Kim <hyangah@gmail.com>
    TryBot-Result: kokoro <noreply+kokoro@google.com>
    Reviewed-by: Suzy Mueller <suzmue@golang.org>
    (cherry picked from commit 2735f152fe31270cdb0595e001d37a66b540f94f)
    Reviewed-on: https://go-review.googlesource.com/c/vscode-go/+/383935
    Reviewed-by: Robert Findley <rfindley@google.com>

--- test/testdata/subTest/sub_test.go
+++ test/testdata/subTest/sub_test.go
@@ -5,6 +5,8 @@ import "testing"
 func TestMain(t *testing.T) {
 	t.Log("Main")
 	t.Run("Sub", func(t *testing.T) { t.Log("Sub") })
+	t.Run("Sub", func(t *testing.T) { t.Log("Sub#01") })
+	t.Run("Sub#01", func(t *testing.T) { t.Log("Sub#01#01") })
 }
 
 func TestOther(t *testing.T) {
