9ea27e69395e3dfb00b43db324eaa24df12bfcf0
Hana (Hyang-Ah) Kim <hyangah@gmail.com>
2022-04-18 23:07:12 -0400

    tools/docs2wiki: tool that rewrites local .md file urls with wiki links
    Unlike markdown style links for local md file references, wiki links
    need to be the url of the wiki pages. In GitHub wiki, links to local
    md files of file.md form send users to in the local disk
    and wiki links are different. In GitHub Wiki, if links to local md files
    send users to raw.github.com pages that serve the raw text content.
    
    https://stackoverflow.com/questions/34804531/in-github-wiki-absolute-links-to-markdown-leads-to-raw-githubusercontent
    
    To avoid this, rewrite local md file uris included in all the md files
    in dir.
    
    And, call this from github action wiki.yml workflow before pushing
    to the wiki repo.
    
    Updates golang/vscode-go#2094
    
    Change-Id: Iaeb756a4c14cd98544a1a58ace859b6f48a358ce
    Reviewed-on: https://go-review.googlesource.com/c/vscode-go/+/401194
    Run-TryBot: Hyang-Ah Hana Kim <hyangah@gmail.com>
    Reviewed-by: Jamal Carvalho <jamal@golang.org>
    TryBot-Result: kokoro <noreply+kokoro@google.com>
    Reviewed-by: Suzy Mueller <suzmue@golang.org>

--- go.mod
+++ go.mod
@@ -3,6 +3,10 @@ module github.com/golang/vscode-go
 go 1.16
 
 require (
+	github.com/google/go-cmp v0.5.7
 	github.com/stamblerre/work-stats v0.0.0-20211013195910-92098c96a21a
 	golang.org/x/build v0.0.0-20211222221018-ee978b38c739
+	golang.org/x/net v0.0.0-20211015210444-4f30a5c0130f // indirect
+	golang.org/x/sys v0.0.0-20211019181941-9d821ace8654 // indirect
+	golang.org/x/text v0.3.7 // indirect
 )
---
+++ tools/docs2wiki/main.go
@@ -0,0 +1,117 @@
+//go:build !windows
+// +build !windows
+
+// Tool docs2wiki rewrites links in ./docs/* to wiki link format.
+// This program may call the 'diff' tool which may be missing on Windows.
+package main
+
+import (
+	"bytes"
+	"flag"
+	"fmt"
+	"io/fs"
+	"io/ioutil"
+	"net/url"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"regexp"
+	"strings"
+)
+
+var writeFlag = flag.Bool("w", false, "Overwrite new file contents to disk.")
+
+func main() {
+	flag.Parse()
+
+	if len(flag.Args()) != 1 {
+		errorf("Usage: %v <dir>", os.Args[0])
+		os.Exit(1)
+	}
+	if err := rewriteLinks(flag.Arg(0), *writeFlag); err != nil {
+		errorf("failed to rewrite links: %v", err)
+		os.Exit(1)
+	}
+}
+
+func rewriteLinks(dir string, overwrite bool) error {
+	return filepath.WalkDir(dir, func(path string, d fs.DirEntry, err error) error {
+		if err != nil {
+			return err
+		}
+		if d.IsDir() {
+			return nil
+		}
+		name := d.Name()
+		if filepath.Ext(name) != ".md" {
+			return nil
+		}
+
+		errorf("processing %v... %v", name, path)
+
+		data, err := ioutil.ReadFile(path)
+		if err != nil {
+			return fmt.Errorf("failed to read file %v: %w", name, err)
+		}
+		converted := markdownLink2WikiLink(data)
+		if overwrite {
+			return ioutil.WriteFile(path, converted, 0644)
+		}
+		tmp, err := writeToTempFile(converted)
+		if err != nil {
+			return fmt.Errorf("failed to write to temp file for diff: %w", err)
+		}
+		defer os.Remove(tmp)
+		diff(path, tmp)
+		return nil
+	})
+}
+
+func diff(f1, f2 string) {
+	cmd := exec.Command("diff", f1, f2)
+	cmd.Stderr = os.Stderr
+	cmd.Stdout = os.Stdout
+	if err := cmd.Run(); err != nil {
+		errorf("failed diff %v %v: %v", f1, f2, err)
+	}
+}
+
+func writeToTempFile(content []byte) (filename string, err error) {
+	dst, err := ioutil.TempFile("", "tmp")
+	if err != nil {
+		return "", fmt.Errorf("failed to write to a temporary file for diff: %v", err)
+	}
+	defer func() {
+		if err == nil {
+			err = dst.Close()
+		}
+	}()
+
+	dst.Write(content)
+	return dst.Name(), nil
+}
+
+// find pattern like '](link.md)'
+var markdownLinkRE = regexp.MustCompile(`\]\(\S+(:?\.md|\.md#[^)]*)\)`)
+
+func markdownLink2WikiLink(src []byte) []byte {
+	return markdownLinkRE.ReplaceAllFunc(src, func(s []byte) []byte {
+
+		part := string(s[2 : len(s)-1]) // remove leading `](` and ending `)`
+		u, err := url.Parse(part)
+		if err != nil {
+			return s
+		}
+		if u.Scheme != "" {
+			return s
+		}
+		u.Path = strings.TrimSuffix(u.Path, ".md")
+		b := &bytes.Buffer{}
+		fmt.Fprintf(b, "](%s)", u.String())
+		return b.Bytes()
+	})
+}
+
+func errorf(format string, a ...interface{}) {
+	fmt.Fprintf(os.Stderr, format+"\n", a...)
+}
