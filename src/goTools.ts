a87c062097677cdac006bc26fa312307e514569f
Hana <hyangah@gmail.com>
2022-04-18 20:49:21 -0400

    src/goTools: install staticcheck@v0.2.2 when using go1.16 or older
    This is tested in test/integration/install.test.ts
    "Install all tools via GOPROXY" test.
    
    Fixes golang/vscode-go#2162
    
    Change-Id: Ie69264cc61977b12bdd41298e1164d6d5adca41f
    Reviewed-on: https://go-review.googlesource.com/c/vscode-go/+/400363
    Run-TryBot: Hyang-Ah Hana Kim <hyangah@gmail.com>
    Reviewed-by: Jamal Carvalho <jamal@golang.org>
    Reviewed-by: Robert Findley <rfindley@google.com>
    TryBot-Result: kokoro <noreply+kokoro@google.com>

--- src/goTools.ts
+++ src/goTools.ts
@@ -86,6 +86,11 @@ export function getImportPathWithVersion(
 			return importPath + '@' + version;
 		}
 	}
+	// staticcheck requires go1.17+ after v0.3.0.
+	// (golang/vscode-go#2162)
+	if (goVersion.lt('1.17') && tool.name === 'staticcheck') {
+		return importPath + '@v0.2.2';
+	}
 	return importPath + '@latest';
 }
 
