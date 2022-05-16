0d481b814f7b45d1c2d1ff7f9009f5d478c99613
Hana (Hyang-Ah) Kim <hyangah@gmail.com>
2022-05-16 15:38:47 -0400

    src/goMain: register VulncheckResultViewProvider
    This will handle *.vulncheck.json files.
    
    Change-Id: I4308daaacd01988abf2db4610ddb24e6cc8d647f
    Reviewed-on: https://go-review.googlesource.com/c/vscode-go/+/406301
    TryBot-Result: kokoro <noreply+kokoro@google.com>
    Run-TryBot: Hyang-Ah Hana Kim <hyangah@gmail.com>
    Reviewed-by: Suzy Mueller <suzmue@golang.org>
    Reviewed-by: Jamal Carvalho <jamal@golang.org>

--- src/goMain.ts
+++ src/goMain.ts
@@ -64,7 +64,7 @@ import extensionAPI from './extensionAPI';
 import { GoTestExplorer, isVscodeTestingAPIAvailable } from './goTest/explore';
 import { killRunningPprof } from './goTest/profile';
 import { GoExplorerProvider } from './goExplorer';
-import { VulncheckProvider } from './goVulncheck';
+import { VulncheckProvider, VulncheckResultViewProvider } from './goVulncheck';
 
 import { GoExtensionContext } from './context';
 import * as commands from './commands';
@@ -170,6 +170,7 @@ export async function activate(ctx: vscode.ExtensionContext): Promise<ExtensionA
 
 	GoExplorerProvider.setup(ctx);
 	VulncheckProvider.setup(ctx, goCtx);
+	VulncheckResultViewProvider.register(ctx);
 
 	registerCommand('go.test.generate.package', goGenerateTests.generateTestCurrentPackage);
 	registerCommand('go.test.generate.file', goGenerateTests.generateTestCurrentFile);
