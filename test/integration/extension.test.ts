f3e8bbe34d8df88683b9976df88ac2d7ed1181b4
unknown <hyangah@gmail.com>
2022-07-21 10:05:09 -0400

    test/integration: catch failed await in lint test
    Often the test failed with timeout. Catch exceptions
    (most likely due to unresolved promise)
    and fail explicitly.
    
      1 failing
        1) Go Extension Tests (GOPATH mode)
               Linting - lint errors with multiple open files:
    	        Error: Timeout of 20000ms exceeded. For async tests and
    		hooks, ensure "done()" is called; if returning a
    		Promise, ensure it resolves.
    
    Change-Id: I894ed74fe52442f036ac3637191b7c1d189cbcf1

--- test/integration/extension.test.ts
+++ test/integration/extension.test.ts
@@ -436,40 +436,45 @@ It returns the number of bytes written and any write error encountered.
 	});
 
 	test('Linting - lint errors with multiple open files', async () => {
-		// handleDiagnosticErrors may adjust the lint errors' ranges to make the error more visible.
-		// This adjustment applies only to the text documents known to vscode. This test checks
-		// the adjustment is made consistently across multiple open text documents.
-		const file1 = await vscode.workspace.openTextDocument(
-			vscode.Uri.file(path.join(fixturePath, 'linterTest', 'linter_1.go'))
-		);
-		const file2 = await vscode.workspace.openTextDocument(
-			vscode.Uri.file(path.join(fixturePath, 'linterTest', 'linter_2.go'))
-		);
-		const warnings = await goLint(
-			file2.uri,
-			Object.create(getGoConfig(), {
-				lintTool: { value: 'staticcheck' },
-				lintFlags: { value: ['-checks', 'all,-ST1000,-ST1016'] }
-				// staticcheck skips debatable checks such as ST1003 by default,
-				// but this test depends on ST1003 (MixedCaps package name) presented in both files
-				// in the same package. So, enable that.
-			}),
-			Object.create(getGoplsConfig(), {}),
-			'package'
-		);
+		try {
+			// handleDiagnosticErrors may adjust the lint errors' ranges to make the error more visible.
+			// This adjustment applies only to the text documents known to vscode. This test checks
+			// the adjustment is made consistently across multiple open text documents.
+			const file1 = await vscode.workspace.openTextDocument(
+				vscode.Uri.file(path.join(fixturePath, 'linterTest', 'linter_1.go'))
+			);
+			const file2 = await vscode.workspace.openTextDocument(
+				vscode.Uri.file(path.join(fixturePath, 'linterTest', 'linter_2.go'))
+			);
+			console.log('start linting');
+			const warnings = await goLint(
+				file2.uri,
+				Object.create(getGoConfig(), {
+					lintTool: { value: 'staticcheck' },
+					lintFlags: { value: ['-checks', 'all,-ST1000,-ST1016'] }
+					// staticcheck skips debatable checks such as ST1003 by default,
+					// but this test depends on ST1003 (MixedCaps package name) presented in both files
+					// in the same package. So, enable that.
+				}),
+				Object.create(getGoplsConfig(), {}),
+				'package'
+			);
 
-		const diagnosticCollection = vscode.languages.createDiagnosticCollection('linttest');
-		handleDiagnosticErrors({}, file2, warnings, diagnosticCollection);
+			const diagnosticCollection = vscode.languages.createDiagnosticCollection('linttest');
+			handleDiagnosticErrors({}, file2, warnings, diagnosticCollection);
 
-		// The first diagnostic message for each file should be about the use of MixedCaps in package name.
-		// Both files belong to the same package name, and we want them to be identical.
-		const file1Diagnostics = diagnosticCollection.get(file1.uri);
-		const file2Diagnostics = diagnosticCollection.get(file2.uri);
-		assert(file1Diagnostics);
-		assert(file2Diagnostics);
-		assert(file1Diagnostics.length > 0);
-		assert(file2Diagnostics.length > 0);
-		assert.deepStrictEqual(file1Diagnostics[0], file2Diagnostics[0]);
+			// The first diagnostic message for each file should be about the use of MixedCaps in package name.
+			// Both files belong to the same package name, and we want them to be identical.
+			const file1Diagnostics = diagnosticCollection.get(file1.uri);
+			const file2Diagnostics = diagnosticCollection.get(file2.uri);
+			assert(file1Diagnostics);
+			assert(file2Diagnostics);
+			assert(file1Diagnostics.length > 0);
+			assert(file2Diagnostics.length > 0);
+			assert.deepStrictEqual(file1Diagnostics[0], file2Diagnostics[0]);
+		} catch (e) {
+			assert.fail(`failed to lint: ${e}`);
+		}
 	});
 
 	test('Error checking', async () => {
