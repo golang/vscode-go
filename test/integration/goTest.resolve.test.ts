a6ca6939d57f9230f52e90855ba71aef18973ad8
unknown <hyangah@gmail.com>
2022-07-20 21:35:53 -0400

    test/integration/goTest: fix populateModulePathCache
    And reenable affected tests.
    
    The tests were broken due to a bug in composing the file Uris for
    files in a directory from the directory Uri. path.join uses the
    platform specific separator (\ on windows). To ensure the correct
    encoding, convert the directory Uri to its fsPath, construct the
    file path of the file, and convert the file path to Uri.
    
    For golang/vscode-go#832
    For golang/vscode-go#1788
    
    Change-Id: I3dfc44e04f1683b1b862b339b7257b432c3f2241

--- test/integration/goTest.resolve.test.ts
+++ test/integration/goTest.resolve.test.ts
@@ -8,7 +8,6 @@ import { GoTestResolver } from '../../src/goTest/resolve';
 import { GoTest, GoTestKind } from '../../src/goTest/utils';
 import { MockTestController, MockTestWorkspace } from '../mocks/MockTest';
 import { getSymbols_Regex, populateModulePathCache } from './goTest.utils';
-import { affectedByIssue832 } from './testutils';
 
 type Files = Record<string, string | { contents: string; language: string }>;
 
@@ -26,9 +25,6 @@ function setup(folders: string[], files: Files) {
 }
 
 suite('Go Test Resolver', () => {
-	if (affectedByIssue832()) {
-		return;
-	}
 	interface TC extends TestCase {
 		item?: ([string, string, GoTestKind] | [string, string, GoTestKind, string])[];
 		expect: string[];
