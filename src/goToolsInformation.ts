1ccf63720984ef65c212a28838b7ce05846abeb9
Hana (Hyang-Ah) Kim <hyangah@gmail.com>
2022-09-13 16:21:39 -0400

    src/goToolsInformation: update gopls latest version
    v0.9.5 is the latest. For vulncheck, it is the desirable
    version too.
    
    Change-Id: I81cd3c2b36a64af7f7d60ad8aa5eb464e2d9be60
    Reviewed-on: https://go-review.googlesource.com/c/vscode-go/+/430716
    Run-TryBot: Hyang-Ah Hana Kim <hyangah@gmail.com>
    Reviewed-by: Suzy Mueller <suzmue@golang.org>

--- src/goToolsInformation.ts
+++ src/goToolsInformation.ts
@@ -211,10 +211,10 @@ export const allToolsInformation: { [key: string]: Tool } = {
 		description: 'Language Server from Google',
 		usePrereleaseInPreviewMode: true,
 		minimumGoVersion: semver.coerce('1.13'),
-		latestVersion: semver.parse('v0.9.3'),
-		latestVersionTimestamp: moment('2022-08-11', 'YYYY-MM-DD'),
-		latestPrereleaseVersion: semver.parse('v0.9.3'),
-		latestPrereleaseVersionTimestamp: moment('2022-08-11', 'YYYY-MM-DD')
+		latestVersion: semver.parse('v0.9.5'),
+		latestVersionTimestamp: moment('2022-09-08', 'YYYY-MM-DD'),
+		latestPrereleaseVersion: semver.parse('v0.9.5'),
+		latestPrereleaseVersionTimestamp: moment('2022-09-08', 'YYYY-MM-DD')
 	},
 	'dlv': {
 		name: 'dlv',
