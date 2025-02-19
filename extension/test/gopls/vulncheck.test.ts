1cd4a9a393261aba9b57844b0b275026ed8d53ab
Hongxiang Jiang <hxjiang@golang.org>
2025-02-19 16:35:42 -0500

    extension/test/gopls: add integration test for gopls.vulncheck
    gopls.vulncheck runs the exact same test as gopls.run_vulncheck.
    
    In production, the gopls.vulncheck streams the log to the terminal
    if terminal remain active in ActiveProgressTerminals. For simplicity,
    we assumed the terminal will never be closed and added to the map
    once created.
    
    For golang/vscode-go#3572
    
    Change-Id: Ie7c94677ee76a10f340d5334e902b4946b2b34dd
    Reviewed-on: https://go-review.googlesource.com/c/vscode-go/+/650646
    LUCI-TryBot-Result: Go LUCI <golang-scoped@luci-project-accounts.iam.gserviceaccount.com>
    kokoro-CI: kokoro <noreply+kokoro@google.com>
    Reviewed-by: Robert Findley <rfindley@google.com>
    Auto-Submit: Hongxiang Jiang <hxjiang@golang.org>
    Reviewed-by: Madeline Kalil <mkalil@google.com>

--- extension/test/gopls/vulncheck.test.ts
+++ extension/test/gopls/vulncheck.test.ts
@@ -6,11 +6,12 @@ import assert from 'assert';
 import path = require('path');
 import sinon = require('sinon');
 import vscode = require('vscode');
-import { ProgressTerminal } from '../../src/progressTerminal';
+import { ActiveProgressTerminals, ProgressTerminal } from '../../src/progressTerminal';
 import { ExecuteCommandParams, ExecuteCommandRequest } from 'vscode-languageserver-protocol';
 import { Env, FakeOutputChannel } from './goplsTestEnv.utils';
 import { URI } from 'vscode-uri';
 import { getGoConfig } from '../../src/config';
+import { ProgressToken } from 'vscode-languageclient';
 
 suite('writeVulns', function () {
 	this.timeout(30000);
@@ -22,8 +23,8 @@ suite('writeVulns', function () {
 	// By reusing the editor session, we reduce the test time by 1.5~2 seconds for each test.
 	const fixtureDir = path.join(__dirname, '..', '..', '..', 'test', 'testdata', 'vuln');
 
-	// Tests will run govulncheck and see if expected output is accumulated in fakeTerminal.
-	const fakeTerminal = new FakeOutputChannel();
+	// Tests will run govulncheck and see if expected output is accumulated in fakeChannel.
+	const fakeChannel = new FakeOutputChannel();
 	suiteSetup(async () => {
 		const config = require('../../src/config');
 		const goConfig = Object.create(getGoConfig(), {
@@ -34,10 +35,19 @@ suite('writeVulns', function () {
 		sandbox.stub(config, 'getGoConfig').returns(goConfig);
 		await env.startGopls(undefined, goConfig, fixtureDir);
 
-		sandbox.stub(ProgressTerminal, 'Open').returns({
-			appendLine: fakeTerminal.appendLine,
-			show: () => {},
-			exit: () => {}
+		sandbox.stub(ProgressTerminal, 'Open').callsFake((_name?: string, token?: ProgressToken) => {
+			const fakeTerminal = {
+				appendLine: fakeChannel.appendLine,
+				show: () => {},
+				exit: () => {}
+			} as ProgressTerminal;
+			if (token) {
+				// Add the fake terminal to ActiveProgressTerminals to stream
+				// logs from executeCommand and workDoneProgress. Test assumes
+				// terminal remains open, unlike production scenarios.
+				ActiveProgressTerminals.set(token, fakeTerminal);
+			}
+			return fakeTerminal;
 		});
 	});
 
@@ -46,10 +56,10 @@ suite('writeVulns', function () {
 			console.log('=== Gopls Trace ===');
 			env.flushTrace(true);
 			console.log('=== Vulncheck Terminal Output ===');
-			console.log(fakeTerminal.toString());
+			console.log(fakeChannel.toString());
 		}
 		env.flushTrace(false);
-		fakeTerminal.clear();
+		fakeChannel.clear();
 	});
 
 	suiteTeardown(async () => {
@@ -57,26 +67,40 @@ suite('writeVulns', function () {
 		sandbox.restore();
 	});
 
-	test('govulncheck finds vulnerabilities', async () => {
+	test('gopls.run_govulncheck finds vulnerabilities', async () => {
 		const workspaceDir = path.join(fixtureDir, 'mod1');
-		const output = await testRunGovulncheck(workspaceDir);
+		const output = await testRunGovulncheck(workspaceDir, 'gopls.run_govulncheck');
 		const result = output.toString();
 		assert(result.includes('GO-1970-TEXT'));
 		assert(result.includes('vulnerabilities found'));
 	});
 
-	test('govulncheck finds no vulnerabilities', async () => {
+	test('gopls.run_govulncheck finds no vulnerabilities', async () => {
 		const workspaceDir = path.join(fixtureDir, 'mod2');
-		const output = await testRunGovulncheck(workspaceDir);
+		const output = await testRunGovulncheck(workspaceDir, 'gopls.run_govulncheck');
 		assert(output.toString().includes('No vulnerabilities found'));
 	});
 
-	async function testRunGovulncheck(workspaceDir: string) {
+	test('gopls.vulncheck finds vulnerabilities', async () => {
+		const workspaceDir = path.join(fixtureDir, 'mod1');
+		const output = await testRunGovulncheck(workspaceDir, 'gopls.vulncheck');
+		const result = output.toString();
+		assert(result.includes('GO-1970-TEXT'));
+		assert(result.includes('vulnerabilities found'));
+	});
+
+	test('gopls.vulncheck finds no vulnerabilities', async () => {
+		const workspaceDir = path.join(fixtureDir, 'mod2');
+		const output = await testRunGovulncheck(workspaceDir, 'gopls.vulncheck');
+		assert(output.toString().includes('No vulnerabilities found'));
+	});
+
+	async function testRunGovulncheck(workspaceDir: string, command: string) {
 		const languageClient = env.languageClient!;
 		const document = await vscode.workspace.openTextDocument(vscode.Uri.file(path.join(workspaceDir, 'go.mod')));
 		const uri = languageClient.code2ProtocolConverter.asTextDocumentIdentifier(document).uri;
 
-		languageClient.middleware!.executeCommand!('gopls.run_govulncheck', [{ URI: uri }], async (cmd, args) => {
+		languageClient.middleware!.executeCommand!(command, [{ URI: uri }], async (cmd, args) => {
 			const params: ExecuteCommandParams = {
 				command: cmd,
 				arguments: args
@@ -89,11 +113,11 @@ suite('writeVulns', function () {
 			const timeout = setTimeout(() => {
 				reject(`Timed out while waiting for '${msg}'`);
 			}, timeoutMS);
-			fakeTerminal.onPattern(msg, () => {
+			fakeChannel.onPattern(msg, () => {
 				clearTimeout(timeout);
 				resolve();
 			});
 		});
-		return fakeTerminal;
+		return fakeChannel;
 	}
 });
