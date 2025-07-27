e35a9e83dbff1422d55ee1f38809464ab30310bf
Maxim Kern <MistaTwista@users.noreply.github.com>
2025-07-27 13:21:52 +0300

    better parsing and use TextDocumentContentProvider
    Also extended Delve context (clipboard) request used for longer strings

--- extension/src/goDebugCommands.ts
+++ extension/src/goDebugCommands.ts
@@ -1,41 +1,96 @@
 import * as vscode from 'vscode';
 
+// Track sessions since vscode doesn't provide a list of them.
+const sessions = new Map<string, vscode.DebugSession>();
+vscode.debug.onDidStartDebugSession((s) => sessions.set(s.id, s));
+vscode.debug.onDidTerminateDebugSession((s) => sessions.delete(s.id));
+
 export function registerGoDebugCommands(ctx: vscode.ExtensionContext) {
-	ctx.subscriptions.push(
-		vscode.commands.registerCommand(
-			'go.debug.openVariableAsDoc',
-			async (args: any) => {
-				const variable = args.variable;
+	class VariableContentProvider implements vscode.TextDocumentContentProvider {
+		static uriForRef(ref: VariableRef) {
+			return vscode.Uri.from({
+				scheme: 'go-debug-variable',
+				authority: `${ref.container.variablesReference}@${ref.sessionId}`,
+				path: `/${ref.variable.name}`
+			});
+		}
 
-				let raw = variable.value.trim();
-				if (
-					(raw.startsWith('"') && raw.endsWith('"')) ||
-					(raw.startsWith('`') && raw.endsWith('`'))
-				) {
-					raw = raw.slice(1, -1);
-				}
-
-				let text: string;
-				try {
-					text = JSON.parse(`"${raw.replace(/"/g, '\\"')}"`);
-				} catch {
-					text = raw
-						.replace(/\\r/g, '\r')
-						.replace(/\\n/g, '\n')
-						.replace(/\\t/g, '\t')
-						.replace(/\\"/g, '"')
-						.replace(/\\\\/g, '\\');
-				}
-
-				const doc = await vscode.workspace.openTextDocument({
-					language: 'plaintext',
-					content: text
-				});
-
-				const editor = await vscode.window.showTextDocument(doc);
-
-				await vscode.commands.executeCommand('workbench.action.editor.changeLanguageMode');
+		async provideTextDocumentContent(uri: vscode.Uri): Promise<string> {
+			const name = uri.path.replace(/^\//, '');
+			const [container, sessionId] = uri.authority.split('@', 2);
+			if (!container || !sessionId) {
+				throw new Error('Invalid URI');
 			}
-		)
-	)
+
+			const session = sessions.get(sessionId);
+			if (!session) return 'Debug session has been terminated';
+
+			const r: { variables: Variable[] } = await session.customRequest('variables', {
+				variablesReference: parseInt(container, 10)
+			});
+
+			const v = r.variables.find((v) => v.name === name);
+			if (!v) return `Cannot resolve variable ${name}`;
+
+			const { result } = await session.customRequest('evaluate', {
+				expression: v.evaluateName,
+				context: 'clipboard'
+			});
+
+			v.value = result ?? v.value;
+
+			return parseVariable(v);
+		}
+	}
+
+	ctx.subscriptions.push(
+		vscode.workspace.registerTextDocumentContentProvider('go-debug-variable', new VariableContentProvider())
+	);
+
+	ctx.subscriptions.push(
+		vscode.commands.registerCommand('go.debug.openVariableAsDoc', async (ref: VariableRef) => {
+			const uri = VariableContentProvider.uriForRef(ref);
+			const doc = await vscode.workspace.openTextDocument(uri);
+			await vscode.window.showTextDocument(doc);
+		})
+	);
+
+	interface VariableRef {
+		sessionId: string;
+		container: Container;
+		variable: Variable;
+	}
+
+	interface Container {
+		name: string;
+		variablesReference: number;
+		expensive: boolean;
+	}
+
+	interface Variable {
+		name: string;
+		value: string;
+		evaluateName: string;
+		variablesReference: number;
+	}
+
+	const escapeCodes: Record<string, string> = {
+		r: '\r',
+		n: '\n',
+		t: '\t'
+	};
+
+	function parseVariable(variable: Variable) {
+		let raw = variable.value.trim();
+		try {
+			// Attempt to parse as JSON
+			return JSON.parse(raw);
+		} catch (_) {
+			// Fall back to manual unescaping
+			raw = raw.slice(1, -1);
+		}
+
+		// Manually unescape
+		return raw.replace(/\\[nrt\\"'`]/, (_, s) => (s in escapeCodes ? escapeCodes[s] : s));
+	}
 }
