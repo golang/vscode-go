bae5a1d4742997aa42a93c04882192d263832bfe
Hongxiang Jiang <hxjiang@golang.org>
2025-08-19 12:48:46 -0400

    survey: add developer survey sub module and release instructions
    The config.json is now pointing at this year's survey waiting to
    be tagged with survey/v0.1.0.
    
    For golang/vscode-go#2891
    
    Change-Id: Ia30e5f56a30085c213347b365b5a823491977f51
    Reviewed-on: https://go-review.googlesource.com/c/vscode-go/+/697395
    Reviewed-by: Madeline Kalil <mkalil@google.com>
    LUCI-TryBot-Result: Go LUCI <golang-scoped@luci-project-accounts.iam.gserviceaccount.com>

---
+++ survey/go.mod
@@ -0,0 +1,3 @@
+module github.com/golang/vscode-go/survey
+
+go 1.24.0
---
+++ survey/survey.go
@@ -0,0 +1,9 @@
+// The survey package holds the config.json file defining the Go Developer
+// Survey configuration.
+//
+// The survey configuration specifies the survey's start and end dates, and the
+// URL for the survey.
+//
+// This package contains no actual Go code, and exists only so the config.json
+// file can be served by module proxies.
+package survey
