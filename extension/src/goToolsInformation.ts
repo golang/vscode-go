30bf47ae6960e5f01ae5bedae106cd63e365cfa6
Gopher Robot <gobot@golang.org>
2025-12-12 07:12:21 -0800

    extension: update gopls v0.21.0 settings
    This is an automated CL which updates the gopls version and settings.
    
    For golang/go#76367
    
    Change-Id: I75f1475bf1cef67589af8ca378f663c4f5c86dd1
    Reviewed-on: https://go-review.googlesource.com/c/vscode-go/+/729661
    Auto-Submit: Gopher Robot <gobot@golang.org>
    LUCI-TryBot-Result: Go LUCI <golang-scoped@luci-project-accounts.iam.gserviceaccount.com>
    Reviewed-by: Alan Donovan <adonovan@google.com>
    Reviewed-by: Madeline Kalil <mkalil@google.com>
    Auto-Submit: Alan Donovan <adonovan@google.com>
    Commit-Queue: Alan Donovan <adonovan@google.com>

--- extension/src/goToolsInformation.ts
+++ extension/src/goToolsInformation.ts
@@ -146,8 +146,8 @@ const internal = new Map<string, Tool>([
 			description: 'Language Server from Google',
 			usePrereleaseInPreviewMode: true,
 			minimumGoVersion: semver.coerce('1.19'),
-			latestVersion: semver.parse('v0.20.0'),
-			latestVersionTimestamp: moment('2025-07-28', 'YYYY-MM-DD')
+			latestVersion: semver.parse('v0.21.0'),
+			latestVersionTimestamp: moment('2025-12-05', 'YYYY-MM-DD')
 		}
 	],
 	[
